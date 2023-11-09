8c5a4a2ab9a1a104cb522949c11c69f003286c91
hojeong14 <rkdghwjd0926@jnu.ac.kr>
2023-11-09 19:06:21 +0900

    라디오 버튼 이벤트 추가

--- part1/src/MainPacage/MainPage.java
+++ part1/src/MainPacage/MainPage.java
@@ -1,7 +1,60 @@
+package MainPacage;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
 import javax.imageio.*;
+
+class CustomRadioButton extends JRadioButton {
+    public CustomRadioButton(String text) {
+        super(text);
+
+        // 라디오 버튼 글꼴
+        setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+        setFocusPainted(false);
+
+        // 마우스 이벤트 처리
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                // 마우스를 올렸을 때 bold체로 설정
+                setFont(new Font("맑은 고딕", Font.BOLD, 18));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // 마우스가 벗어났을 때 원래대로 설정
+                ButtonModel model = getModel();
+                if (!model.isSelected()) {
+                    setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+                }
+            }
+        });
+
+        // 이벤트 리스너 추가
+        addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 클릭 시 글꼴을 bold체로 설정
+                setFont(new Font("맑은 고딕", Font.BOLD, 18));
+
+                // 다른 라디오 버튼을 선택하면 현재 라디오 버튼의 글꼴을 원래대로 설정
+                ButtonModel model = getModel();
+                if (!model.isSelected()) {
+                    setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+                }
+            }
+        });
+
+        // 아이콘 변경 시 글꼴을 원래대로 설정
+        addChangeListener(e -> {
+            if (!getModel().isSelected()) {
+                setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+            }
+        });
+    }
+}
 
 public class MainPage extends JFrame {
     public MainPage() {
@@ -13,7 +66,7 @@ public class MainPage extends JFrame {
         //타이틀 아이콘 변경
         try {
             // 이미지 파일을 읽어와서 아이콘 이미지로 설정
-            Image iconImage = ImageIO.read(MainPage.class.getResource("image/logo.jpg"));
+            Image iconImage = ImageIO.read(MainPage.class.getResource("../image/logo.jpg"));
             setIconImage(iconImage);
         } catch (Exception e) {
             // 예외 처리
@@ -32,84 +85,80 @@ public class MainPage extends JFrame {
         leftPanel.setBackground(Color.WHITE);
         leftPanel.setLayout(new GridBagLayout()); // GridBagLayout을 사용
         GridBagConstraints gbc = new GridBagConstraints();
-        GridBagConstraints gbc1 = new GridBagConstraints();
 
         //난이도 선택 라디오 버튼
         ButtonGroup levelGroup = new ButtonGroup();
-        JRadioButton level1 = new JRadioButton("하");
-        JRadioButton level2 = new JRadioButton("중");
-        JRadioButton level3 = new JRadioButton("상");
+        CustomRadioButton level1 = new CustomRadioButton("하");
+        CustomRadioButton level2 = new CustomRadioButton("중");
+        CustomRadioButton level3 = new CustomRadioButton("상");
 
         //font
-
-//        level1.setFont(customFont);
         level1.setBackground(Color.WHITE);
-//        level2.setFont(customFont);
         level2.setBackground(Color.WHITE);
-//        level3.setFont(customFont);
         level3.setBackground(Color.WHITE);
 
         levelGroup.add(level1);
         levelGroup.add(level2);
         levelGroup.add(level3);
 
-        gbc.gridwidth = 1;
-        gbc.insets = new Insets(0, 5, 0, 0);
-        gbc.fill = GridBagConstraints.NONE;
-        leftPanel.add(level1,gbc);
-        leftPanel.add(level2,gbc);
-        leftPanel.add(level3,gbc);
+//        gbc.fill = GridBagConstraints.NONE;
+        gbc.gridx = 0;
+        gbc.gridy=0;
+        gbc.insets = new Insets(0, 0, 0, 60);
+        leftPanel.add(level1, gbc);
+        gbc.insets = new Insets(0, 100, 0, 30);
+        leftPanel.add(level2, gbc);
+        gbc.insets = new Insets(0, 200, 0, 10);
+        leftPanel.add(level3, gbc);
 
         //게임 시작 label
         JLabel label_gameStart;
         try {
-            Image imageFile = ImageIO.read(MainPage.class.getResource("image/triangle.png"));
-            Image imgTriangle = imageFile.getScaledInstance(38, 38, Image.SCALE_SMOOTH);
+            Image imageFile = ImageIO.read(MainPage.class.getResource("../image/yongyong2.png"));
+            Image imgTriangle = imageFile.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             ImageIcon image_triangle = new ImageIcon(imgTriangle);
             label_gameStart = new JLabel("게임 시작", image_triangle, SwingConstants.CENTER);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.gridwidth = 3;
-        gbc.insets = new Insets(0, 0, 30, 0);
-//        gbc.fill = GridBagConstraints.HORIZONTAL;
-//        gbc.anchor = GridBagConstraints.SOUTHWEST;
-//        gbc.anchor = GridBagConstraints.LAST_LINE_START;
-        label_gameStart.setFont(new Font("맑은 고딕", Font.BOLD, 40));
+        gbc.insets = new Insets(0, 0, 20, 0);
+        label_gameStart.setFont(new Font("맑은 고딕", Font.BOLD, 45));
         leftPanel.add(label_gameStart, gbc);
 
         JLabel label_infoPage;
         try {
-            Image imageFile = ImageIO.read(MainPage.class.getResource("image/triangle.png"));
-            Image imgTriangle = imageFile.getScaledInstance(38, 38, Image.SCALE_SMOOTH);
+            Image imageFile = ImageIO.read(MainPage.class.getResource("../image/yongyong2.png"));
+            Image imgTriangle = imageFile.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             ImageIcon image_triangle = new ImageIcon(imgTriangle);
             label_infoPage = new JLabel("정보 열람", image_triangle, SwingConstants.CENTER);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         gbc.gridx = 0;
-        gbc.gridy = 2;
+        gbc.gridy = 3;
         gbc.anchor = GridBagConstraints.LAST_LINE_START;
         leftPanel.add(label_infoPage, gbc);
-        label_infoPage.setFont(new Font("맑은 고딕", Font.BOLD, 40));
+        label_infoPage.setFont(new Font("맑은 고딕", Font.BOLD, 45));
 
         JLabel label_setting;
         try {
-            Image imageFile = ImageIO.read(MainPage.class.getResource("image/triangle.png"));
-            Image imgTriangle = imageFile.getScaledInstance(38, 38, Image.SCALE_SMOOTH);
+            Image imageFile = ImageIO.read(MainPage.class.getResource("../image/yongyong2.png"));
+            Image imgTriangle = imageFile.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             ImageIcon image_triangle = new ImageIcon(imgTriangle);
             label_setting = new JLabel("설정", image_triangle, SwingConstants.CENTER);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         gbc.gridx = 0;
-        gbc.gridy = 3;
+        gbc.gridy = 4;
         gbc.gridwidth = 1;
         gbc.anchor = GridBagConstraints.LAST_LINE_START;
         leftPanel.add(label_setting, gbc);
-        label_setting.setFont(new Font("맑은 고딕", Font.BOLD, 40));
+        label_setting.setFont(new Font("맑은 고딕", Font.BOLD, 45));
 
 
         // 오른쪽 패널에 이미지 추가 (예제에서는 빈 레이블로 대체)
@@ -119,7 +168,7 @@ public class MainPage extends JFrame {
 
         //이미지 삽입
         try {
-            Image main_page_logo = ImageIO.read(MainPage.class.getResource("image/yongyong1.png"));
+            Image main_page_logo = ImageIO.read(MainPage.class.getResource("../image/yongyong1.png"));
             Image img_logo_scal = main_page_logo.getScaledInstance(350, 350, Image.SCALE_SMOOTH);
             ImageIcon img_logo = new ImageIcon(img_logo_scal);
             JLabel main_logo = new JLabel(img_logo);
